82530f72173165dced5b81d20118ef66597dc67a
matthias.prankl@josephinum.at <matthias.prankl@josephinum.at>
2022-09-14 12:00:23 +0200

    Erstes "echtes" Projekt gemacht

---
+++ 01_ValueTypesAndReferenceTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using _01_ValueTypesAndReferenceTypes;
+
+void ChangeValue(int x)
+{
+    x = 200;            //lokale Variable eine value types --> Änderungen bleiben lokal
+    Console.WriteLine(x);
+}
+
+void ChangeValueReferenceType(Student s)
+{
+    s.Name = "Julian";
+}
+
+int i = 100;
+Console.WriteLine(i);
+ChangeValue(i);
+Console.WriteLine(i);
+
+Student tobias = new Student();
+tobias.Name = "Tobias";
+Console.WriteLine(tobias);
+ChangeValueReferenceType(tobias);
+Console.WriteLine(tobias);    
+
