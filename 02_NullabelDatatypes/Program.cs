e18faf96f17cc619cb2b1910caed5125b830fce1
matthias.prankl@josephinum.at <matthias.prankl@josephinum.at>
2022-09-14 12:19:44 +0200

    Programme geschrieben

---
+++ 02_NullabelDatatypes/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using _02_NullabelDatatypes;
+
+student tobias = null;      //reference type can be null
+
+//Console.WriteLine(tobias.Name);   --> NullReferenceException, weil Tobias null ist
+
+if(tobias != null)
+{
+    Console.WriteLine(tobias.Name);
+}
+
+int? i = null;          //nullable Datentyp int --> durch ?
+i = 5;                  //Zuweisung zu einem nullable Datatype
+
+int j = i.Value * 4;
+
+
