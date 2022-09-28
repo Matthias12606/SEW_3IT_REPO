48c5bb095a3ef00e042283eece44dbba42049001
matthias.prankl@josephinum.at <matthias.prankl@josephinum.at>
2022-09-28 12:21:16 +0200

    TryParse

---
+++ 04_TryParse/Program.cs
@@ -0,0 +1,15 @@
+﻿int i = int.Parse("523");
+//int j = int.Parse("Hello World");         //Laufzeitfehler
+int n;
+
+Console.Write("Bitte eine Zahl eingeben: ");
+string input = Console.ReadLine();  
+
+bool parsed = int.TryParse("HelloWorld", out n);
+if (parsed == false)
+{
+    Console.WriteLine("Konnte Eingabe nicht in Zahl umwandeln");
+}else
+{
+    Console.WriteLine(n);
+}
