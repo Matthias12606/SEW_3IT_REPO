a2895f70b3b7d680d9c97519f30b882aed191bde
matthias.prankl@josephinum.at <matthias.prankl@josephinum.at>
2022-09-28 11:54:37 +0200

    03 Gemacht

--- 02_NullabelDatatypes/Program.cs
+++ 02_NullabelDatatypes/Program.cs
@@ -17,6 +17,24 @@ i = 5;                  //Zuweisung zu einem nullable Datatype
 int j = i.Value * 4;        //Zugriff auf i.Value nur wenn nicht null
 
 i = null;
-j = i.Value * 4; 
+if (i.HasValue)              //Zugriff auf i.Value nur wenn nicht null
+{
+    j = i.Value * 4;
+}
+else
+{
+    j = 0;
+}
+
+//entweder
+
+i = 6;
+if (i.HasValue)         //Abfrage darf man auch weglassen, wenn ich sicher bin, dass i einen Wert hat
+{
+    j = i.Value * 5;
+}
 
 
+//oder
+
+j = i.GetValueOrDefault() * 5;          //leifert den Wert wenn nicht NULL, sonst Default-Wert (0)
---
+++ 03_CallByValueCallByReference/Program.cs
@@ -0,0 +1,61 @@
+﻿using _03_CallByValueCallByReference;
+
+void SwapD(double a, double b)
+{
+    double temp = a;
+    a = b;
+    b = temp;
+}
+
+void SwapDRef(ref double a, ref double b)
+{
+    double temp = a;
+    a = b;
+    b = temp;
+}
+
+void SwapS( Student a, Student b)
+{
+    Student temp = a;
+    a = b;
+    b = temp;
+}
+
+void SwapSRef(ref Student a, ref Student b)
+{
+    Student temp = a;
+    a = b;
+    b = temp;
+}
+
+void SwapNameS(Student a, Student b)
+{
+    string temp = a.Name;
+    a.Name = b.Name;
+    b.Name = temp;
+}
+
+double a = 5.0;
+double b = 4.0;
+
+SwapD(a, b);                // a und b werden nicht vertauscht, weil Argumente als Parameter übergeben und nach außen sichtbar sind
+Console.WriteLine("a = " + a);
+Console.WriteLine("b = " + b);
+
+Student matthias = new Student() { Name = "Matthias" };
+Student florian = new Student() { Name = "Florian" };
+SwapS(matthias, florian);           // a und b werden nicht vertauscht, weil Argumente als Parameter übergeben und nach außen sichtbar sind
+Console.WriteLine("matthias = " + matthias);
+Console.WriteLine("florian = " + florian);
+
+SwapNameS(matthias, florian);       //a und b werden vertauscht, weil die "Änderung" auf Datenkomponenten des Objekts stattfinden
+Console.WriteLine("Matthias = " + matthias);
+Console.WriteLine("Florian = " + florian);
+
+SwapDRef(ref a, ref b);            // a und b werden vertauscht, weil Argumente als Referenc (ref) übergeben werden
+Console.WriteLine("a = " + a);
+Console.WriteLine("b = " + b);
+
+SwapSRef(ref matthias, ref florian);         // matthias und florian werden vertauscht, weil Argumente als Referenc (ref) übergeben werden
+Console.WriteLine("matthias = " + matthias);
+Console.WriteLine("florian = " + florian);
