705c76d694a24f823a51139e418543aec95c0266
matthias.prankl@josephinum.at <matthias.prankl@josephinum.at>
2022-10-07 11:25:32 +0200

    Pause

---
+++ 05Namespaces/Program.cs
@@ -0,0 +1,9 @@
+﻿using FJ.IT;
+
+FJ.IT.Student Matteo = new FJ.IT.Student();
+FJ.Ag.Student Ag = new FJ.Ag.Student();
+
+Student philip = new Student();                 //wenn der Namespace im using ist, dann brauch ich keine volle Qualifizierung (FJ.IT nicht notwendig)
+FJ.Ag.Student hannah = new FJ.Ag.Student(); ;   //wenn der Namespace nicht im using, dann volle Qualifizierung
+
+
